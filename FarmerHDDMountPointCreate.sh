7b1dd3d085de3bb2239a32238d702bddf142ba89
Trina Shih <trina.shih@gmail.com>
2021-07-10 15:02:20 -0700

    Farmer HDD mount point create

---
+++ FarmerHDDMountPointCreate.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#1
+
+#sudo mkdir /mnt/sd
+
+sudo mkdir /mnt/sdb
+sudo mkdir /mnt/sdc
+sudo mkdir /mnt/sdd
+sudo mkdir /mnt/sde 
+sudo mkdir /mnt/sdf 
+sudo mkdir /mnt/sdg 
+sudo mkdir /mnt/sdh 
+sudo mkdir /mnt/sdi 
+sudo mkdir /mnt/sdj 
+sudo mkdir /mnt/sdk 
+sudo mkdir /mnt/sdl 
+sudo mkdir /mnt/sdm 
+sudo mkdir /mnt/sdn 
+sudo mkdir /mnt/sdo 
+sudo mkdir /mnt/sdp 
+sudo mkdir /mnt/sdq 
+sudo mkdir /mnt/sdr 
+sudo mkdir /mnt/sds 
+sudo mkdir /mnt/sdt 
+sudo mkdir /mnt/sdu 
+sudo mkdir /mnt/sdv 
+sudo mkdir /mnt/sdw
+sudo mkdir /mnt/sdx
+sudo mkdir /mnt/sdy
+sudo mkdir /mnt/sdz
+sudo mkdir /mnt/sdaa
+sudo mkdir /mnt/sdab
+sudo mkdir /mnt/sdac
+sudo mkdir /mnt/sdad
+sudo mkdir /mnt/sdae
+sudo mkdir /mnt/sdaf
+sudo mkdir /mnt/sdag
+sudo mkdir /mnt/sdah
+sudo mkdir /mnt/sdai
+sudo mkdir /mnt/sdaj
+sudo mkdir /mnt/sdak
+sudo mkdir /mnt/sdal
+sudo mkdir /mnt/sdam
+sudo mkdir /mnt/sdan
+sudo mkdir /mnt/sdao
+sudo mkdir /mnt/sdap
+sudo mkdir /mnt/sdaq
+sudo mkdir /mnt/sdar
+sudo mkdir /mnt/sdas 
+sudo mkdir /mnt/sdat  
