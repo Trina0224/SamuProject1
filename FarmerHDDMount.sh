ce5dc061e21481aaecc4498eddf9ed6e13bb8565
Trina Shih <trina.shih@gmail.com>
2021-07-10 15:11:10 -0700

    Farmer HDD Mount

---
+++ FarmerHDDMount.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#1
+
+#sudo mount /dev/sd1 /mnt/sd 
+
+sudo mount  /dev/sdb1 /mnt/sdb
+sudo mount /dev/sdc1 /mnt/sdc 
+sudo mount /dev/sdd1 /mnt/sdd
+sudo mount /dev/sde1 /mnt/sde 
+sudo mount /dev/sdf1 /mnt/sdf
+sudo mount /dev/sdg1 /mnt/sdg
+sudo mount /dev/sdh1 /mnt/sdh
+sudo mount /dev/sdi1 /mnt/sdi
+sudo mount /dev/sdj1 /mnt/sdj
+sudo mount /dev/sdk1 /mnt/sdk
+sudo mount /dev/sdl1 /mnt/sdl
+sudo mount /dev/sdm1 /mnt/sdm
+sudo mount /dev/sdn1 /mnt/sdn
+sudo mount /dev/sdo1 /mnt/sdo
+sudo mount /dev/sdp1 /mnt/sdp
+sudo mount /dev/sdq1 /mnt/sdq
+sudo mount /dev/sdr1 /mnt/sdr
+sudo mount /dev/sds1 /mnt/sds
+sudo mount /dev/sdt1 /mnt/sdt
+sudo mount /dev/sdu1 /mnt/sdu
+sudo mount /dev/sdv1 /mnt/sdv
+sudo mount /dev/sdw1 /mnt/sdw
+sudo mount /dev/sdx1 /mnt/sdx
+sudo mount /dev/sdy1 /mnt/sdy
+sudo mount /dev/sdz1 /mnt/sdz
+
+#sudo mount /dev/sda1 /mnt/sda
+sudo mount /dev/sdaa1 /mnt/sdaa
+sudo mount /dev/sdab1 /mnt/sdab
+sudo mount /dev/sdac1 /mnt/sdac
+sudo mount /dev/sdad1 /mnt/sdad
+sudo mount /dev/sdae1 /mnt/sdae
+sudo mount /dev/sdaf1 /mnt/sdaf
+sudo mount /dev/sdag1 /mnt/sdag
+sudo mount /dev/sdah1 /mnt/sdah
+sudo mount /dev/sdai1 /mnt/sdai
+sudo mount /dev/sdaj1 /mnt/sdaj
+sudo mount /dev/sdak1 /mnt/sdak
+sudo mount /dev/sdal1 /mnt/sdal
+sudo mount /dev/sdam1 /mnt/sdam
+sudo mount /dev/sdan1 /mnt/sdan
+sudo mount /dev/sdao1 /mnt/sdao
+sudo mount /dev/sdap1 /mnt/sdap
+sudo mount /dev/sdaq1 /mnt/sdaq
+sudo mount /dev/sdar1 /mnt/sdar
+sudo mount /dev/sdas1 /mnt/sdas
+sudo mount /dev/sdat1 /mnt/sdat
+
+
+sudo chown -R `whoami` /mnt/*
