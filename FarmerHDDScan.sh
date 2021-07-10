dc544292fd15ca571f9b794fdfc2608e06c61576
Trina Shih <trina.shih@gmail.com>
2021-07-10 14:41:21 -0700

    farmer hdd scan

---
+++ FarmerHDDScan.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+#1
+echo "sda"
+sudo smartctl -i /dev/sda | grep -i "User Capacity"
+
+echo "sdb"
+sudo smartctl -i /dev/sdb | grep -i "User Capacity"
+
+echo "sdc"
+sudo smartctl -i /dev/sdc | grep -i "User Capacity"
+
+echo "sdd"
+sudo smartctl -i /dev/sdd | grep -i "User Capacity"
+
+echo "sde"
+sudo smartctl -i /dev/sde | grep -i "User Capacity"
+
+echo "sdf"
+sudo smartctl -i /dev/sdf | grep -i "User Capacity"
+
+echo "sdg"
+sudo smartctl -i /dev/sdg | grep -i "User Capacity"
+
+echo "sdh"
+sudo smartctl -i /dev/sdh | grep -i "User Capacity"
+
+echo "sdi"
+sudo smartctl -i /dev/sdi | grep -i "User Capacity"
+
+echo "sdj"
+sudo smartctl -i /dev/sdj | grep -i "User Capacity"
+
+echo "sdk"
+sudo smartctl -i /dev/sdk | grep -i "User Capacity"
+
+echo "sdl"
+sudo smartctl -i /dev/sdl | grep -i "User Capacity"
+
+echo "sdm"
+sudo smartctl -i /dev/sdm | grep -i "User Capacity"
+
+echo "sdn"
+sudo smartctl -i /dev/sdn | grep -i "User Capacity"
+
+echo "sdo"
+sudo smartctl -i /dev/sdo | grep -i "User Capacity"
+
+echo "sdp"
+sudo smartctl -i /dev/sdp | grep -i "User Capacity"
+
+echo "sdq"
+sudo smartctl -i /dev/sdq | grep -i "User Capacity"
+
+echo "sdr"
+sudo smartctl -i /dev/sdr | grep -i "User Capacity"
+
+echo "sds"
+sudo smartctl -i /dev/sds | grep -i "User Capacity"
+
+echo "sdt"
+sudo smartctl -i /dev/sdt | grep -i "User Capacity"
+
+echo "sdu"
+sudo smartctl -i /dev/sdu | grep -i "User Capacity"
+
+echo "sdv"
+sudo smartctl -i /dev/sdv | grep -i "User Capacity"
+
+echo "sdw"
+sudo smartctl -i /dev/sdw | grep -i "User Capacity"
+
+echo "sdx"
+sudo smartctl -i /dev/sdx | grep -i "User Capacity"
+
+echo "sdy"
+sudo smartctl -i /dev/sdy | grep -i "User Capacity"
+
+echo "sdz"
+sudo smartctl -i /dev/sdz | grep -i "User Capacity"
+
+#27
+echo "sdaa"
+sudo smartctl -i /dev/sdaa | grep -i "User Capacity"
+
+echo "sdab"
+sudo smartctl -i /dev/sdab | grep -i "User Capacity"
+
+echo "sdac"
+sudo smartctl -i /dev/sdac | grep -i "User Capacity"
+
+echo "sdad"
+sudo smartctl -i /dev/sdad | grep -i "User Capacity"
+
+echo "sdae"
+sudo smartctl -i /dev/sdae | grep -i "User Capacity"
+
+echo "sdaf"
+sudo smartctl -i /dev/sdaf | grep -i "User Capacity"
+
+echo "sdag"
+sudo smartctl -i /dev/sdag | grep -i "User Capacity"
+
+echo "sdah"
+sudo smartctl -i /dev/sdah | grep -i "User Capacity"
+
+echo "sdai"
+sudo smartctl -i /dev/sdai | grep -i "User Capacity"
+
+echo "sdaj"
+sudo smartctl -i /dev/sdaj | grep -i "User Capacity"
+
+echo "sdak"
+sudo smartctl -i /dev/sdak | grep -i "User Capacity"
+
+echo "sdal"
+sudo smartctl -i /dev/sdal | grep -i "User Capacity"
+
+echo "sdam"
+sudo smartctl -i /dev/sdam | grep -i "User Capacity"
+
+echo "sdan"
+sudo smartctl -i /dev/sdan | grep -i "User Capacity"
+
+echo "sdao"
+sudo smartctl -i /dev/sdao | grep -i "User Capacity"
+
+echo "sdap"
+sudo smartctl -i /dev/sdap | grep -i "User Capacity"
+
+#43
+echo "sdaq"
+sudo smartctl -i /dev/sdaq | grep -i "User Capacity"
+
+echo "sdar"
+sudo smartctl -i /dev/sdar | grep -i "User Capacity"
+
+echo "sdas"
+sudo smartctl -i /dev/sdas | grep -i "User Capacity"
+
+#46??
+echo "sdat"
+sudo smartctl -i /dev/sdat | grep -i "User Capacity"
